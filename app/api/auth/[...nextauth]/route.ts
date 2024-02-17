0abec6afa11ddc9cce7a3a712b85a670118f158e
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2024-02-17 21:53:56 +0000

    chore: re-enable providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import LinkedInProvider from "next-auth/providers/linkedin";
+import TwitterProvider from "next-auth/providers/twitter";
 
 const handler = NextAuth({
   pages: {
@@ -10,19 +13,19 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    // TwitterProvider({
-    //   clientId: process.env.TWITTER_ID as string,
-    //   clientSecret: process.env.TWITTER_SECRET as string,
-    //   version: "2.0"
-    // }),
-    // GitHubProvider({
-    //   clientId: process.env.GITHUB_ID as string,
-    //   clientSecret: process.env.GITHUB_SECRET as string,
-    // }),
-    // LinkedInProvider({
-    //   clientId: process.env.LINKEDIN_ID as string,
-    //   clientSecret: process.env.LINKEDIN_SECRET as string,
-    // }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID as string,
+      clientSecret: process.env.TWITTER_SECRET as string,
+      version: "2.0"
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_ID as string,
+      clientSecret: process.env.LINKEDIN_SECRET as string,
+    }),
   ],
 });
 

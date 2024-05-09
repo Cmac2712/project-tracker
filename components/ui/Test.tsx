f728ee69fa09562abec46b30089bec08cd6e6aea
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2024-05-09 22:08:34 +0100

    fix: errors

--- components/ui/Test.tsx
+++
@@ -1,7 +0,0 @@
-import { getUser } from "@/app/lib/actions/getUser"
-
-export default async function Test() {
-    const user = await getUser('cmac2712@gmail.com')
-
-    return <p>{`This is a test component. ${user ? user.name : 'friend'}`}</p>
-}
